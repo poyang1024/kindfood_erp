bb2f64dbf0dcc437e30902297260c394380e806f
poyang1024 <poyang1024@gmail.com>
2025-11-17 17:54:39 +0800

    refactor: replace hardcoded Firebase configuration with environment variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjDntWW060i__-VRwMkaWUsHuvTHrke64",
-  authDomain: "kindfood-erp.firebaseapp.com",
-  projectId: "kindfood-erp",
-  storageBucket: "kindfood-erp.appspot.com",
-  messagingSenderId: "867024882608",
-  appId: "1:867024882608:web:d794ed87a419a8556fa475"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 
